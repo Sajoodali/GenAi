5fe20205379347712b95f18f06b7c777be9781f9
sajoodali <sajoodali486@gmail.com>
2026-04-09 23:35:54 +0500

    data base connected

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
 
-async function connectDB() {
+async function connectToDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const uri = process.env.MONGO_URI;
+        if (!uri) {
+            throw new Error("MONGO_URI is not defined. Make sure .env is loaded before connectToDB() is called.");
+        }
+        await mongoose.connect(uri);
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("MongoDB connection error:", error);
@@ -14,4 +15,4 @@ async function connectDB() {
     }
 }
 
-export default connectDB;
+export default connectToDB;
