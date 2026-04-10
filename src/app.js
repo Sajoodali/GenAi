1b8dedf63aa39bb1de147991105c024d7d56eba3
sajoodali <sajoodali486@gmail.com>
2026-04-11 00:16:35 +0500

    logout token black listing

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express  from "express";
 import authrouter from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 // Auth routes
 app.use("/api/auth", authrouter);
